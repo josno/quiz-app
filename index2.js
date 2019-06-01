d1fd6426124f4364b08f6b23df866eeccb372f91
Joanne Senoren <jnsenoren@gmail.com>
2019-06-01 20:39:32 +0900

    doing the questions

--- index2.js
+++ index2.js
@@ -1,3 +1,58 @@
+const STORE = [
+    {
+        question: "Blood sugar is also known as?",
+        answer: "Glucose",
+        choices: ["Frutose","Glucose","Cellulose"],
+    },
+    {
+        question: "Food with a high glycemic index means:",
+        answer: "it contains alot of glucose.",
+        choices: ["it contains alot of glucose.","there is a net amount of glucose.","there is a high amount of fat."],
+    },
+    {
+        question: "Diabetics can eat:",
+        answer: "All of the above",
+        choices: ["Fish, meats, cheese, ice cream","Meat, sugar-free food, nuts","Meat, fruits, potatoes","All of the above","None of the above"],
+    },
+    {
+        question: "You are more likely to get diabetes if:",
+        answer: "your parent has it.",
+        choices: ["your parent has it.","you are overweight.","you eat a lot of sugar."],
+    },
+    {
+        question: "What's makes Type 1 different from Type 2?",
+        answer: "Your body doesn't make its own insulin.",
+        choices: ["It's the good one.","Your body doesn't make its own insulin.","Your body is resistant to insulin.", "You get it when your are young."],
+    },
+    {
+        question: "Low blood sugar is also called:",
+        answer: "Hypoglycemia",
+        choices: ["Hypoglycemia","Hyperglycemia","Hypothyroidism", "Hypothalamus"],
+    },
+    {
+        question: "What's one way to raise blood sugar quickly?",
+        answer: "Drink some juice.",
+        choices: ["Eat a steak.","Eat some nuts.","Drink some water.","Drink some juice."],
+    },
+    {
+        question: "How many grams of carbohydrates is in a 375ml can of Coke?",
+        answer: "39.8",
+        choices: ["39.8","21.5","45.6","38.9"],
+    },
+    {
+        question: "How does carbohydrates affect diabetes?",
+        answer: "It raises your blood sugar level.",
+        choices: ["It doesn't do anything.","It raises your blood sugar level.","It drops your blood sugar level."],
+    },
+    {
+        question: "What does insulin do to our blood sugar?",
+        answer: "It regulates it.",
+        choices: ["It raises it.","It regulates it.","It gets rid of blood sugar."],
+    },
+
+]
+
+
 function startQuiz() {
 // Button should start the quiz 
 // Hide the start
@@ -10,6 +65,33 @@ function startQuiz() {
 };
 
 function getQuestion(){
+    STORE.forEach(item => 
+        $('.question-form').append(`
+        <div class='boxes question-box'>
+                <form>
+                    <legend>
+                        <h2>Question #<span>1</span></h2>
+                        <p>${item.question}</p>
+                    </legend>
+
+                    <p>
+                    <input type="radio" value="xx" name="answer" required>
+                    <label>Fructose</label>
+                    </p>
+
+                    <p>
+                    <input type="radio" value="xx" name="answer" required>
+                    <label>Glucose</label>
+                    </p>
+
+                    <p>
+                    <input type="radio" value="xx" name="answer" required>
+                    <label>Cellulose</label>
+                    </p>
+                
+                    <button type="button" class="btn-style">Check My Answer</button>
+                </form>`)
+    );
     // gets the question from the array and returns in HTML format
     // return html tags to append to .html file; use string template
     //consider
@@ -18,6 +100,7 @@ function getQuestion(){
 
     // $('#question-text').text(questionText)
     // Add questions; consider forEach
+    console.log('getQuestion ran');
 };
 
 function quizPlay() {
@@ -97,4 +180,5 @@ quizEnd()
 restart()
 */
 startQuiz()
+getQuestion()
 
